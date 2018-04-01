c318231f0e4c446acefff1688df9ef671cca45db
spchampa <st.champa@gmail.com>
2018-03-31 19:03:19 -0600

    basic login finished

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,104 @@
-let express = require('express');
-let router = express.Router();
-
-
-
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
 
 // Get Register View
-router.get('/register', function(req, res){
+router.get('/register', function (req, res) {
     res.render('register');
 });
 
 // Get Login View
-router.get('/login', function(req, res){
+router.get('/login', function (req, res) {
     res.render('login');
 });
 
+// Register User
+router.post('/register', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    // Validation
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register', {
+            errors: errors
+        });
+    } else {
+        var newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        });
+
+        User.createUser(newUser, function (err, user) {
+            if (err) throw err;
+            console.log(user);
+        });
+
+        req.flash('success_msg', 'Registration Successful.');
+
+        res.redirect('/users/login');
+    }
+});
+
+// checks login attempts username and password.
+passport.use(new LocalStrategy(function (username, password, done) {
+    // uses mongodb functions to find if user is in the db
+    User.getUserByUsername(username, function (err, user) {
+
+        if (err) throw err;
+        if (!user) {
+            //if not a match
+            return done(null, false, { message: 'Unknown User' });
+        }
+        // if a username match is found --> compare password from mongo
+        User.comparePassword(password, user.password, function (err, isMatch) {
+            if (err) throw err;
+            if (isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Invalid password' });
+            }
+        });
+    });
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.getUserById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+router.post('/login',
+    passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
+    function (req, res) {
+        res.redirect('/');
+    });
+
+router.get('/logout', function (req, res) {
+    req.logout();
+
+    req.flash('success_msg', 'You are logged out');
+
+    res.redirect('/users/login');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -83,6 +83,7 @@ app.use(function (req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null; // determines if user is logged in or not
     next();
 });
 
