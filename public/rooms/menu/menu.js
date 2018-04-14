5c52f6961ce0201522bc4f9f06da179539370e90
blitz555 <chi.zhang@ucalgary.ca>
2018-04-14 12:34:44 -0600

    Update menu.js

--- public/rooms/menu/menu.js
+++ public/rooms/menu/menu.js
@@ -1,115 +1,155 @@
-let roomsArray;
-let rooms_count = 0;
+let rooms_count=4;
 let canvas_room_str = "Canvas Room ";
-window.onload = loadRooms();
+let add_button = $('#btn-add');
+let last_button = $('#btn4');
 
-function loadRooms(){
+$('#btn').on('click', function(){
+    $("#includedContent").load("/rooms/canvas.html");
+    setIntent($('#btn').text());
+})
 
-	console.log('load rooms');
-	var pathname = window.location.pathname; // Returns path only
-	let home_url = window.location.href;     // Returns full URL
-	console.log(pathname);
-	console.log(home_url);
-	$.ajax({
-		url: "/rooms.json",
-		async: false,
-		dataType: 'json',
-		success: function(data) {
-			roomsArray = data;
-			rooms_count = roomsArray.length;
-		}
-	});
+$('#btn-make').on('click', function(){
+    socket.emit('makeRoom', {
+        'name': $('#btn').text(),
+        'path': 'stock_image.jpg'
+    })
+})	
 
-	console.log(roomsArray.length);
-	var i;
-	for(i = 0; i < roomsArray.length; i++){
-		let element = $('<li>');
-		let element_add = $('#btn-add-li');
-		let myButton = document.createElement('button');
-		rooms_count = i + 1;
-		let button_id = 'btn' + rooms_count;
-		let button_txt = canvas_room_str + rooms_count;
-		let button_class = '';
-		if(rooms_count%5===0){
-			button_class = "\"btn btn-secondary\"";
-		}
-		else if(rooms_count%5===1){
-			button_class = "\"btn btn-primary\"";
-		}
-		else if(rooms_count%5===2){
-			button_class = "\"btn btn-success\"";
-		}
-		else if(rooms_count%5===3){
-			button_class = "\"btn btn-warning\"";
-		}
-		else{
-			button_class = "\"btn btn-danger\"";
-		}
-		//console.log(button_class);
-		myButton = "<button id =" + button_id + " type=\"button\" class="+ button_class +">" + button_txt + "</button>";
+$('#btn2').on('click', function(){
+    $("#includedContent").load("/rooms/canvas.html");
+    setIntent($('#btn2').text());
+})
 
-		element.append(myButton);
-		element.insertBefore(element_add);
+$('#btn2-make').on('click', function(){
+    socket.emit('makeRoom', {
+        'name': $('#btn2').text(),
+        'path': 'stock_apple.png'
+    })
+})
 
-		document.getElementById(button_id).addEventListener('click', function(){
-			$("#includedContent").load("/rooms/canvases/canvas.html");
-			setIntent($('#'+button_id).text());
-		})
+$('#btn3').on('click', function(){
+    $("#includedContent").load("/rooms/canvas.html");
+    setIntent($('#btn3').text());
+})
 
-		// socket.emit('makeRoom', {
-		//     'name': canvas_room_str + rooms_count,
-		//     'path': 'stock_apple.png'
-		// })
-	}
-}
+$('#btn3-make').on('click', function(){
+    socket.emit('makeRoom', {
+        'name': $('#btn3').text(),
+        'path': 'stock_image.jpg'
+    })
+})
 
-//not used?
+$('#btn4').on('click', function(){
+    $("#includedContent").load("/rooms/canvas.html");
+    setIntent($('#btn4').text());
+})
+
+$('#btn4-make').on('click', function(){
+    socket.emit('makeRoom', {
+        'name': $('#btn4').text(),
+        'path': 'stock_apple.png'
+    })
+})
+
 function gotoroom(room){
-	let room_str = room;
-	$('#btn4').on('click', function(){
-		$("#includedContent").load("/rooms/canvas.html");
-		setIntent($('#btn4').text());
-	})
+    let room_str = room;
+    $('#btn4').on('click', function(){
+        $("#includedContent").load("/rooms/canvas.html");
+        setIntent($('#btn4').text());
+    })
 }
+
+// $('button').on('click', function(){
+//     //if()
+//
+//     $("#includedContent").load("/rooms/canvas.html");
+//     setIntent($('#btn4').text());
+// })
+
 
 $('#btn-add').on('click', function(){
-	rooms_count++;//increment rooms count
-	console.log(canvas_room_str + rooms_count);
+    rooms_count++;//increment rooms count
+    console.log(canvas_room_str + rooms_count);
 
-	let element = $('<li>');
-	let element_add = $('#btn-add-li');
-	let myButton = document.createElement('button');
-	let button_id = 'btn' + rooms_count;
-	let button_txt = canvas_room_str + rooms_count;
-	let button_class = '';
-	if(rooms_count%5===0){
-		button_class = "\"btn btn-secondary\"";
-	}
-	else if(rooms_count%5===1){
-		button_class = "\"btn btn-primary\"";
-	}
-	else if(rooms_count%5===2){
-		button_class = "\"btn btn-success\"";
-	}
-	else if(rooms_count%5===3){
-		button_class = "\"btn btn-warning\"";
-	}
-	else{
-		button_class = "\"btn btn-danger\"";
-	}
-	//console.log(button_class);
-	myButton = "<button id =" + button_id + " type=\"button\" class="+ button_class +">" + button_txt + "</button>";
+    let element = $('<li>');
+    let element_add = $('#btn-add-li');
+    let myButton = document.createElement('button');
+    let button_id = 'btn' + rooms_count;
+    let button_txt = canvas_room_str + rooms_count;
+    let button_class = '';
+    if(rooms_count%5===0){
+        button_class = "\"btn btn-secondary\"";
+    }
+    else if(rooms_count%5===1){
+        button_class = "\"btn btn-primary\"";
+    }
+    else if(rooms_count%5===2){
+        button_class = "\"btn btn-success\"";
+    }
+    else if(rooms_count%5===3){
+        button_class = "\"btn btn-warning\"";
+    }
+    else{
+        button_class = "\"btn btn-danger\"";
+    }
+    //console.log(button_class);
+    myButton = "<button id =" + button_id + " type=\"button\" class="+ button_class +">" + button_txt + "</button>";
 
-	element.append(myButton);
-	element.insertBefore(element_add);
+    element.append(myButton);
+    element.insertBefore(element_add);
 
-	document.getElementById(button_id).addEventListener('click', function(){
-		$("#includedContent").load("/rooms/canvases/canvas.html");
-		setIntent($('#'+button_id).text());
-	})
+    document.getElementById(button_id).addEventListener('click', function(){
+        $("#includedContent").load("/rooms/canvas.html");
+        setIntent($('#'+button_id).text());
+    })
 
-	socket.emit('makeRoom', {
-		'name': canvas_room_str + rooms_count,
-		'path': 'stock_apple.png'
-	})
+    socket.emit('makeRoom', {
+        'name': canvas_room_str + rooms_count,
+        'path': 'stock_apple.png'
+    })
+})
+
+$('#createRoomBtn').on('click', function(){
+    let roomName = document.getElementById('roomNameInput').value;
+    console.log(roomName);
+
+    rooms_count++;//increment rooms count
+    console.log(canvas_room_str + rooms_count);
+
+    let element = $('<li>');
+    let element_add = $('#btn-add-li');
+    let myButton = document.createElement('button');
+    let button_id = 'btn' + rooms_count;
+    let button_txt = canvas_room_str + rooms_count;
+    let button_class = '';
+    if(rooms_count%5===0){
+        button_class = "\"btn btn-secondary\"";
+    }
+    else if(rooms_count%5===1){
+        button_class = "\"btn btn-primary\"";
+    }
+    else if(rooms_count%5===2){
+        button_class = "\"btn btn-success\"";
+    }
+    else if(rooms_count%5===3){
+        button_class = "\"btn btn-warning\"";
+    }
+    else{
+        button_class = "\"btn btn-danger\"";
+    }
+    //console.log(button_class);
+    myButton = "<button id =" + button_id + " type=\"button\" class="+ button_class +">" + roomName + "</button>";
+
+    element.append(myButton);
+    element.insertBefore(element_add);
+
+    document.getElementById(button_id).addEventListener('click', function(){
+        $("#includedContent").load("/rooms/canvas.html");
+        setIntent(btn_txt);
+    })
+
+    socket.emit('makeRoom', {
+        'name': canvas_room_str + rooms_count,
+        'path': 'stock_apple.png'
+    })
 })
